05e71c1268efc88d386560c25a21c95a51962767
Tarun Kakani <watchdog@Taruns-MacBook-Air.local>
2024-12-09 20:31:59 +0530

    incomplete

---
+++ main.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>LoginPHP</title>
+</head>
+<body>
+    <form method="post" action="main.php">
+        Username:<br>
+        <input type="text" name="user">
+
+        Password:<br>
+        <input type="password" name="pass">
+    </form>
+
+    <?php
+        
+    ?>
+</body>
+</html>
