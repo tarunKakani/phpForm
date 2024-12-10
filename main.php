129f8210cfced2a6b5f2d43d88a4187b178e146d
Tarun Kakani <watchdog@Taruns-MacBook-Air.local>
2024-12-10 17:16:58 +0530

    updated code

--- main.php
+++ main.php
@@ -3,19 +3,69 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>LoginPHP</title>
+    <title>Login</title>
 </head>
 <body>
-    <form method="post" action="main.php">
+    <h2>Login Form PHP</h2>
+    
+    <form method="post" action="<?php htmlspecialchars($_SERVER["PHP_SELF"]) ?>"> 
         Username:<br>
-        <input type="text" name="user">
+        <input type="text" name="user"><br>
 
         Password:<br>
-        <input type="password" name="pass">
+        <input type="password" name="pass"><br>
+
+        <button type="submit" name="login">Login</button>
     </form>
 
     <?php
-        
+        $db_server = "localhost";
+        $db_username = "root";
+        $db_password = "";
+        $db_name = "Students";
+
+        try{
+            $conn = mysqli_connect($db_server, $db_username, $db_password, $db_name);
+            echo "Connection Successful <br>";
+        }
+        catch(mysqli_sql_exception $e){
+            echo "Connection Failed: " . $e->getMessage() . "<br>";
+        }
+
+        // This is better
+        if ($_SERVER["REQUEST_METHOD"] == "POST"){
+            $username = filter_input(INPUT_POST, "user",FILTER_SANITIZE_SPECIAL_CHARS);
+            $password = filter_input(INPUT_POST, "pass",FILTER_SANITIZE_SPECIAL_CHARS);
+
+            if (empty($username)){
+                echo "Please enter a username!";
+            }
+
+            elseif(empty($password)){
+                echo "Please enter a password";
+            }
+
+            else{
+                $hash = password_hash($password,PASSWORD_DEFAULT);
+                $sql = "INSERT INTO users (username,password) VALUES ('$username','$hash')";
+
+                try{
+                    mysqli_query($conn,$sql);
+                    echo "Database updated";
+                }
+                catch(mysqli_sql_exception $e){
+                    echo "Database not updated: " . $e->getMessage() . "<br>";
+                }
+            }
+        }
+
+        // Old
+        // if (isset($_POST["login"])){
+        //     if (isset($_POST["user"])){
+        //         $username = $_POST["user"];
+        //         echo $username;
+        //     }
+        // }
     ?>
 </body>
 </html>
